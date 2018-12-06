f6cc538eae2896eb9cfc8a456c12ca998d4c0880
bassmky <gitbassmky19940208@gmail.com>
2018-12-07 00:11:19 +0900

    Initial Commit

---
+++ Swift5Introduction1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Introduction1
+//
+//  Created by 奥川幹也 on 2018/12/07.
+//  Copyright © 2018 奥川幹也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
